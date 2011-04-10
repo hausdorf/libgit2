36888b0287c4ff8a592493b7872c3741a01b93bd
Joy Hui <joy.chenhuan.hui@gmail.com>
2011-04-09 18:52:26 -0600

    Remove extraneous comments, make struct style consistent

--- src/diff.h
+++ src/diff.h
@@ -25,19 +25,11 @@ typedef struct {
     char *records_changed;
 } git_diff_data;
 
-typedef struct s_middle_edit {
+typedef struct {
     // begin..end of sequences a, b
     const long *begin_a, *end_a, *begin_b, *end_b;
     // the difference value of begin b, a
     const long *k;
 } middle_edit;
-/**
- * @brief Find minimal paths by bisecting the sequences a, b
- */
-
-/**
- * @brief
- */
-
 
 #endif
