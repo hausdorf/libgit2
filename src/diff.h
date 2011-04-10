06053d5d1a396522be59edf8ca60614157c6149c
Alex Clemmer <clemmer.alexander@gmail.com>
2011-04-10 01:59:15 -0600

    Remove extraneous comment

--- src/diff.h
+++ src/diff.h
@@ -3,18 +3,14 @@
 
 #include "git2/diff.h"
 
-/**
- * \struct git_diff_data
- * Represents the metadata for the diff
- */
 struct git_diffdata {
-	/*! The number of records (lines) in the content */
+	/* The number of records (lines) in the content */
 	long num_records;
-	/*! Ordered list of hashed records */
+	/* Ordered list of hashed records */
 	unsigned long const *hashed_records;
-	/*! The index of the record in the content */
+	/* The index of the record in the content */
 	long *record_index;
-	/*! The set of records that have changed, represented by 1: changed */
+	/* The set of records that have changed, represented by 1: changed */
 	char *records_changed;
 };
 
