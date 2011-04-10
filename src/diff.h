d47d98a8d2d8655954f21630bd9648280591033b
Joy Hui <joy.chenhuan.hui@gmail.com>
2011-04-09 19:05:54 -0600

    Move internal struct from diff.h to diff.c to increase encapsulation

--- src/diff.h
+++ src/diff.h
@@ -25,11 +25,4 @@ typedef struct {
     char *records_changed;
 } git_diff_data;
 
-typedef struct {
-    // begin..end of sequences a, b
-    const long *begin_a, *end_a, *begin_b, *end_b;
-    // the difference value of begin b, a
-    const long *k;
-} middle_edit;
-
 #endif
