8bee6d03644d943b2a159b957e76f8574308b84f
trane <trane@xmission.com>
2011-04-12 20:11:51 -0600

    Change spaces to tabs

--- src/diff.c
+++ src/diff.c
@@ -28,28 +28,28 @@
 int git_diff_no_index(git_diffdata **diffdata, const char *filename1,
 		const char *filename2)
 {
-    return 0;
+	return 0;
 }
 
 /* TODO TODO TODO: THIS NEEDS TO BE IMPLEMENTED */
 int git_diff(git_diffdata **diffdata, git_commit *commit,
 		git_repository *repo)
 {
-    return 0;
+	return 0;
 }
 
 /* TODO TODO TODO: THIS NEEDS TO BE IMPLEMENTED */
 int git_diff_cached(git_diffdata **diffdata, git_commit *commit,
 		git_index *index)
 {
-    return 0;
+	return 0;
 }
 
 /* TODO TODO TODO: THIS NEEDS TO BE IMPLEMENTED */
 int git_diff_commits(git_diffdata **diffdata, git_commit *commit1,
 		git_commit *commit2)
 {
-    return 0;
+	return 0;
 }
 
 /*!
