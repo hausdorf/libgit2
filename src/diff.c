a82f0a5c837f92226001d8719560895884fb0b65
Landon GB <landogbland@gmail.com>
2011-04-16 11:56:09 -0600

    Fix tabs and implements git_diff_commits()

--- src/diff.c
+++ src/diff.c
@@ -38,62 +38,62 @@ typedef struct {
  * the caller or a memory leak will occur */
 static int load_file(char *file_path, char *buffer, int *size)
 {
-    FILE *file;
-    file = fopen(file_path, "rb");
+	FILE *file;
+	file = fopen(file_path, "rb");
 
-    if(!file)
-        return appropiate_error;
+	if(!file)
+		return appropiate_error;
 
-    fseek(file, 0, SEEK_END);
-    size=ftell(file);
-    fseek(file, 0, SEEK_SET);
+	fseek(file, 0, SEEK_END);
+	size=ftell(file);
+	fseek(file, 0, SEEK_SET);
 
-    /* Allocate memory */
-    buffer=(char *)malloc(size+1);
-    if (!buffer)
-        return GIT_ENOMEM;
+	/* Allocate memory */
+	buffer=(char *)malloc(size+1);
+	if (!buffer)
+		return GIT_ENOMEM;
 
-    /* Read file contents into buffer */
-    fread(buffer, size, 1, file);
-    fclose(file);
+	/* Read file contents into buffer */
+	fread(buffer, size, 1, file);
+	fclose(file);
 
-    return GIT_SUCCESS;
+	return GIT_SUCCESS;
 }
 
 int git_diff_no_index(git_diffdata **diffdata, const char *filename1,
 		const char *filename2)
 {
-    char *buffer1 = NULL;
-    char *buffer2 = NULL;
-    int buffer1_size, buffer2_size;
-    int result = GIT_SUCCESS;
+	char *buffer1 = NULL;
+	char *buffer2 = NULL;
+	int buffer1_size, buffer2_size;
+	int result = GIT_SUCCESS;
 
-    /* Insure all paramater are valid */
-    if(!file1 | !file2) {
-        result = appropiate_error;
-        goto cleanup;
-    }
+	/* Insure all paramater are valid */
+	if(!file1 | !file2) {
+		result = appropiate_error;
+		goto cleanup;
+	}
 
-    if(!load_file(file1, buffer1, &buffer1_size))
-        result = appropiate_error;
-        goto cleanup;
-    }
+	if(!load_file(file1, buffer1, &buffer1_size))
+		result = appropiate_error;
+		goto cleanup;
+	}
 
-    if(!load_file(file2, buffer2, &buffer2_size)) {
-        result = appropiate_error;
-        goto cleanup;
-    }
+	if(!load_file(file2, buffer2, &buffer2_size)) {
+		result = appropiate_error;
+		goto cleanup;
+	}
 
-    /* call diff on file1, file2 (don't forget to malloc diffdata) */
-    diff();
+	/* call diff on file1, file2 (don't forget to malloc diffdata) */
+	diff();
 
 cleanup:
-    if(buffer1)
-        free(buffer1);
-    if(buffer2)
-        free(buffer2);
+	if(buffer1)
+		free(buffer1);
+	if(buffer2)
+		free(buffer2);
 
-    return GIT_SUCCESS;
+	return GIT_SUCCESS;
 }
 
 /* The git_oid is the sha1 identifier for a blob. Therefore, i think the
@@ -101,133 +101,133 @@ cleanup:
  * filesystem and compare it to this git_oid. */
 static int compare_hashes(char *filename, git_oid *blob_id, int *result)
 {
-    char *file;
-    int file_size;
-    git_oid file_id; /* The resulting SHA1 hash of the file */
+	char *file;
+	int file_size;
+	git_oid file_id; /* The resulting SHA1 hash of the file */
 
-    if(load_file(filename, file, &file_size)  < 0)
-        return GIT_ENOMEM;
+	if(load_file(filename, file, &file_size)  < 0)
+		return GIT_ENOMEM;
 
-    /* Compare the two git_oid */
-    git_hash_buf(&file_id, (void *) file, file_size);
-    if(file_id == *blob_id)
-        *result = 1;
-    else
-        *result = 0;
+	/* Compare the two git_oid */
+	git_hash_buf(&file_id, (void *) file, file_size);
+	if(file_id == *blob_id)
+		*result = 1;
+	else
+		*result = 0;
 
-    /* Cleanup */
-    free(file);
-    return GIT_SUCCESS;
+	/* Cleanup */
+	free(file);
+	return GIT_SUCCESS;
 }
 
 /* Prone to change, maybe easier to pass repo in then char* location */
 static int file_exists(char *filename, char *location)
 {
-    return 0;
+	return 0;
 }
 
 /* 0 if success, error otherwise */
 static int get_git_tree(git_tree *results, git_commit *commit)
 {
-    git_oid *tree_id;
-    git_reference *reference;
+	git_oid *tree_id;
+	git_reference *reference;
 
-    /* Get the tree for this diff, head if commit is null, else the commit
-     * tree */
-    if(!commit) {
-        if(git_reference_lookup(&reference, repo, "HEAD") < GIT_SUCCESS)
-            return approperate_error;
+	/* Get the tree for this diff, head if commit is null, else the commit
+	 * tree */
+	if(!commit) {
+		if(git_reference_lookup(&reference, repo, "HEAD") < GIT_SUCCESS)
+			return approperate_error;
 
-        tree_id = git_reference_oid(head);
-        if(git_tree_lookup(&tree, repo, tree_id) < GIT_SUCCESS)
-            return apporperate_error;
-    }
-    else {
-        if(git_commit_tree(&tree, commit) < GIT_SUCCESS)
-            return approperate_error;
-    }
+		tree_id = git_reference_oid(head);
+		if(git_tree_lookup(&tree, repo, tree_id) < GIT_SUCCESS)
+			return apporperate_error;
+	}
+	else {
+		if(git_commit_tree(&tree, commit) < GIT_SUCCESS)
+			return approperate_error;
+	}
 
-    return GIT_SUCCESS;
+	return GIT_SUCCESS;
 }
 
 /* 0 on success, error on failure. The resulting char* must be freed by the
  * caller */
 static int get_filepath(char* results, git_repository *repo,
-                        git_tree_entry *entry)
+						git_tree_entry *entry)
 {
-        filepath = malloc (char *) sizeof(char) *
-                   (strlen(git_repository_workdir(repo))
-                   + strlen(git_tree_entry_name(entry)));
-        if(filepath == NULL)
-            return GIT_ENOMEM;
+		filepath = malloc (char *) sizeof(char) *
+				   (strlen(git_repository_workdir(repo))
+				   + strlen(git_tree_entry_name(entry)));
+		if(filepath == NULL)
+			return GIT_ENOMEM;
 
-        strcat(filepath, git_repository_workdir(repo));
-        strcat(filepath, git_tree_entry_name(entry));
+		strcat(filepath, git_repository_workdir(repo));
+		strcat(filepath, git_tree_entry_name(entry));
 
-        return GIT_SUCCESS;
+		return GIT_SUCCESS;
 }
 
 int git_diff(git_diffdata **diffdata, git_commit *commit, git_repository *repo)
 {
-    git_tree *tree;             /* The tree that we will be diffing */
-    git_tree_entry *entry;      /* Enteries in the tree we are diffing */
-    char *filepath;             /* filepath to a file in the working directory*/
-    int compare_results;        /* results from compare hash */
-    int error_status;           /* error status of this method */
-    int i;                      /* loop counter */
+	git_tree *tree;			 /* The tree that we will be diffing */
+	git_tree_entry *entry;	  /* Enteries in the tree we are diffing */
+	char *filepath;			 /* filepath to a file in the working directory*/
+	int compare_results;		/* results from compare hash */
+	int error_status;		   /* error status of this method */
+	int i;					  /* loop counter */
 
-    error_status = GIT_SUCCESS;
+	error_status = GIT_SUCCESS;
 
-    /* Get the tree we will be diffing */
-    if(get_git_tree(tree, commit) < 0) {
-        error_status = ERROR;
-        goto cleanup;
-    }
+	/* Get the tree we will be diffing */
+	if(get_git_tree(tree, commit) < 0) {
+		error_status = ERROR;
+		goto cleanup;
+	}
 
-    /* Compare the blobs in this tree with the files in the local filesystem */
-    for(i=0; i<get_tree_entrycount(tree); i++) {
-        entry = git_tree_entry_byindex(tree, i);
+	/* Compare the blobs in this tree with the files in the local filesystem */
+	for(i=0; i<get_tree_entrycount(tree); i++) {
+		entry = git_tree_entry_byindex(tree, i);
 
-        if(get_filepath(filepath, repo, entry) < 0) {
-            error_status = GIT_ENOMEM;
-            goto cleanup;
-        }
+		if(get_filepath(filepath, repo, entry) < 0) {
+			error_status = GIT_ENOMEM;
+			goto cleanup;
+		}
 
-        if(file_exists(filepath)) {
-            if(compare_hashes(filepath, blob, &compare_results) < 0) {
-                error_status = GIT_ENOMEM;
-                goto cleanup;
-            }
+		if(file_exists(filepath)) {
+			if(compare_hashes(filepath, blob, &compare_results) < 0) {
+				error_status = GIT_ENOMEM;
+				goto cleanup;
+			}
 
-            if(!compare_results)
-                diff();
-        }
-        else {
-            /* The file has been deleted from the file system */
-        }
+			if(!compare_results)
+				diff();
+		}
+		else {
+			/* The file has been deleted from the file system */
+		}
 
-        free(filepath);
-    }
+		free(filepath);
+	}
 
-    /* Check every file on the local filesystem, to catch any new files that
-     * may have been created sense the commit */
-    for(each file in filesystem) {
-        entry = git_tree_entry_byname(tree, filename);
+	/* Check every file on the local filesystem, to catch any new files that
+	 * may have been created sense the commit */
+	for(each file in filesystem) {
+		entry = git_tree_entry_byname(tree, filename);
 
-        if(entry == NULL) {
-            /* This is a newly created file sense the commit we are diffing */
-        }
-    }
+		if(entry == NULL) {
+			/* This is a newly created file sense the commit we are diffing */
+		}
+	}
 
 cleanup:
-    if(filepath)
-        free(filepath);
-    if(tree)
-        git_tree_close(tree);
-    if(files_hash)
-        git_hashtable_free(files_hash);
+	if(filepath)
+		free(filepath);
+	if(tree)
+		git_tree_close(tree);
+	if(files_hash)
+		git_hashtable_free(files_hash);
 
-    return error_status;
+	return error_status;
 }
 
 
@@ -235,33 +235,71 @@ int git_diff_cached(git_diffdata **diffdata, git_commit *commit,
 		git_index *index) {}
 
 
+/* Assumes commit1 is newer then commit 2 */
 int git_diff_commits(git_diffdata **diffdata, git_commit *commit1,
 		git_commit *commit2)
 {
-    git_tree *tree1, *tree2;
-    git_tree_entry *entry;
-    int results;
-    int i;
+	git_tree *tree1, *tree2;
+	git_tree_entry *entry1, *entry2;
+	git_oid *blob1, *blob2;
+	char *filename;
+	int results;
+	int i;
 
-    results = GIT_SUCCESS;
+	results = GIT_SUCCESS;
 
-    /* Get the trees for this diff */
-    if(git_commit_tree(&tree1, commit1) < GIT_SUCCESS)
-        return approperate_error;
-    if(git_commit_tree(&tree2, commit2) < GIT_SUCCESS)
-        return approperate_error;
+	/* Get the trees for this diff */
+	if(git_commit_tree(&tree1, commit1) < GIT_SUCCESS) {
+		results = some_error;
+		goto cleanup;
+	}
+	if(git_commit_tree(&tree2, commit2) < GIT_SUCCESS) {
+		results = some_error;
+		goto cleanup;
+	}
 
-    /* Compare the blobs in these trees looking for differences
-     * TODO - make sure there are only filenames in the tree
-     * TODO - make sure that the every file from both tree is checked and
-     *        accounted for */
-    for(int i=0; i<get_tree_entrycount(tree); i++) {
-    }
+	/* Compare the blobs in these trees looking for differences */
+	for(i=0; i<get_tree_entrycount(tree1); i++) {
+		entry1 = git_tree_entry_byindex(tree1, i);
+		filename = git_tree_entry_name(entry1);
 
-    /* Cleanup */
-    git_tree_close(tree1);
-    git_tree_close(tree2);
-    return GIT_SUCCESS;
+		/* Entry1 is guarenteed to exist, Entry2 may or may not exist */
+		entry2 = git_tree_entry_byname(tree2, filename);
+
+		if(!entry2) {
+			/* this entry got deleted between commit1 and commit2 */
+		}
+		else {
+			/* Compare the SHA1's of both blobs to see if the file has
+			 * changed */
+			blob1 = git_tree_entry_id(entry1);
+			blob2 = git_tree_entry_id(entry1);
+			if(*blob1 == *blob2)
+				diff();
+		}
+	}
+
+	/* Go through tree2 to check for files that were added between
+	 * these commits */
+	for(i=0; i<get_tree_entrycount(tree2); i++) {
+		entry2 = git_tree_entry_byindex(tree2, i);
+		filename = git_tree_entry_name(entry2);
+
+		/* Entry2 is guarenteed to exist, Entry1 may or may not exist */
+		entry1 = git_tree_entry_byname(tree2, filename);
+
+		if(!entry1) {
+			/* this entry got added between commit1 and commit2 */
+		}
+	}
+
+cleanup:
+	if(tree1)
+		git_tree_close(tree1);
+	if(tree2)
+		git_tree_close(tree2);
+
+	return results;
 }
 
 
@@ -300,7 +338,7 @@ int xdl_recs_cmp(git_diffdata *dd1, long off1, long lim1,
 		 * Divide ...
 		 */
 		/*if (xdl_split(ha1, off1, lim1, ha2, off2, lim2, kvdf, kvdb,
-			      need_min, &spl, xenv) < 0) {
+				  need_min, &spl, xenv) < 0) {
 
 			return -1;
 		}*/
@@ -310,7 +348,7 @@ int xdl_recs_cmp(git_diffdata *dd1, long off1, long lim1,
 		 */
 		/*if (xdl_recs_cmp(dd1, off1, spl.i1, dd2, off2, spl.i2,
 				 kvdf, kvdb, spl.min_lo, xenv) < 0 ||
-		    xdl_recs_cmp(dd1, spl.i1, lim1, dd2, spl.i2, lim2,
+			xdl_recs_cmp(dd1, spl.i1, lim1, dd2, spl.i2, lim2,
 				 kvdf, kvdb, spl.min_hi, xenv) < 0) {
 
 			return -1;
