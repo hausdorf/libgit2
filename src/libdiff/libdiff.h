3bbec0911bd1d885a3c7db8382c6404c3f86a8e2
Alex Clemmer <clemmer.alexander@gmail.com>
2011-04-16 23:42:39 -0600

    Remove TODO; libgit2 innards should NOT use Doxygen comments

--- src/libdiff/libdiff.h
+++ src/libdiff/libdiff.h
@@ -1,8 +1,7 @@
 #ifndef INCLUDE_libdiff_h__
 #define INCLUDE_libdiff_h__
 
-// TODO: find out if Doxygen comments should be used internally like so
-/*!
+/*
  * Represents file data (binary or text) in memory.
  * Where the data variable points to user data and size is the size of that data
  * This is used to hold the data from one diff file.
