38cb58d21628fff9f743a0462304ec54eac577e3
Alex Clemmer <clemmer.alexander@gmail.com>
2011-04-16 23:42:03 -0600

    Source diff-critical struct to libdiff.h; include link in src/diff.h

--- src/diff.c
+++ src/diff.c
@@ -1,18 +1,5 @@
 #include "diff.h"
-
-// TODO: find out if Doxygen comments should be used internally like so
-/*!
- * Represents file data (binary or text) in memory.
- * Where the data variable points to user data and size is the size of that data
- * This is used to hold the data from one diff file.
- */
-struct git_diff_mem {
-	long size;
-	char *data;
-};
-
-typedef struct git_diff_mem git_diff_m_data;
-typedef struct git_diff_mem git_diff_m_buffer;
+#include "libdiff/libdiff.h"
 
 /* TODO TODO TODO: THIS NEEDS TO BE IMPLEMENTED */
 int git_diff_no_index(git_diffresults_conf **results_conf, const char *filename1,
