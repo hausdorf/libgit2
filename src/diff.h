c472bdab105a4ed106a65efecd8869260df81d3f
Alex Clemmer <clemmer.alexander@gmail.com>
2011-04-16 22:28:08 -0600

    Remove old irrelevant struct, replace with dummy struct

--- src/diff.h
+++ src/diff.h
@@ -4,14 +4,20 @@
 #include "git2/diff.h"
 
 struct git_diffresults_conf {
-	/* The number of records (lines) in the content */
+}
+
+// TODO: REMOVE
+// Keeping this because it's a good readable reference of what
+// the corresponding xdiff/git struct is SUPPOSED to do.
+/*struct git_diffresults_conf {
+	// The number of records (lines) in the content
 	long num_records;
-	/* Ordered list of hashed records */
+	// Ordered list of hashed records
 	unsigned long const *hashed_records;
-	/* The index of the record in the content */
+	// The index of the record in the content
 	long *record_index;
-	/* The set of records that have changed, represented by 1: changed */
+	// The set of records that have changed, represented by 1: changed
 	char *records_changed;
-};
+};*/
 
 #endif
