6522e1de00d6e2e12b6fc16ad5bcf2a38221eb14
Alex Clemmer <clemmer.alexander@gmail.com>
2011-04-17 10:01:23 -0600

    Source the structs that are really a part of libdiff to libdiff.h

--- src/diff.h
+++ src/diff.h
@@ -3,43 +3,6 @@
 
 #include "git2/diff.h"
 
-/*
- * Represents file data (binary or text) in memory. Often
- * instances of these structs are the direct objects that
- * diffing is performed on.
- *
- * Typedef'd as both git_diff_m_data and git_diff_m_buffer,
- * which are identical in concept and execution, but
- * conceptually different.
- */
-struct git_diff_mem {
-	long size;
-	char *data;
-};
-typedef struct git_diff_mem git_diff_m_data;
-typedef struct git_diff_mem git_diff_m_buffer;
-
-/*
- * Callback function; called directly after we perform the
- * diff and the output is assembled according to options
- * specified in the git_diff_out_conf struct.
- */
-struct git_diff_callback {
-	void *payload;
-	int (*out_func)(void *, git_diff_m_buffer *, int);
-};
-typedef struct git_diff_callback git_diff_callback;
-
-/*
- * Fully configures not just how we generate diff's results,
- * but also *where* they go, and what form they take.
- */
-struct git_diffresults_conf {
-	unsigned long flags;
-	git_diff_callback *callback;
-};
-
-
 // TODO: REMOVE
 // Keeping this because it's a good readable reference of what
 // the corresponding xdiff/git struct is SUPPOSED to do.
--- src/libdiff/libdiff.h
+++ src/libdiff/libdiff.h
@@ -1,6 +1,45 @@
 #ifndef INCLUDE_libdiff_h__
 #define INCLUDE_libdiff_h__
 
+#include "../common.h"
+
+/*
+ * Represents file data (binary or text) in memory. Often
+ * instances of these structs are the direct objects that
+ * diffing is performed on.
+ *
+ * Typedef'd as both git_diff_m_data and git_diff_m_buffer,
+ * which are identical in concept and execution, but
+ * conceptually different.
+ */
+struct git_diff_mem {
+	long size;
+	char *data;
+};
+typedef struct git_diff_mem git_diff_m_data;
+typedef struct git_diff_mem git_diff_m_buffer;
+
+/*
+ * Callback function; called directly after we perform the
+ * diff and the output is assembled according to options
+ * specified in the git_diff_out_conf struct.
+ */
+struct git_diff_callback {
+	void *payload;
+	int (*out_func)(void *, git_diff_m_buffer *, int);
+};
+typedef struct git_diff_callback git_diff_callback;
+
+/*
+ * Fully configures not just how we generate diff's results,
+ * but also *where* they go, and what form they take.
+ */
+struct git_diffresults_conf {
+	// TODO: find out if this should be a size_t
+	unsigned long flags;
+	git_diff_callback *callback;
+};
+
 int diff(git_diff_m_data *data1, git_diff_m_data *data2,
 		git_diffresults_conf const *results_conf);
 
