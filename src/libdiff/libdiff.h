93b50f73df484d0f8d708f708749aaa8a865775e
Alex Clemmer <clemmer.alexander@gmail.com>
2011-04-21 12:37:54 -0600

    Add struct to libdiff.h

--- src/libdiff/libdiff.h
+++ src/libdiff/libdiff.h
@@ -6,6 +6,23 @@
 
 
 
+// TODO: COMMENT HERE
+struct diffdata {
+	long num_recs;
+	// This is DEFINITELY an array of hashes; in
+	// xdl_recs_cmp we access it as such
+	unsigned long const *hshd_recs;
+	// Points to diff_context.keys: Array of longs that represent hashes,
+	// used to access weights[]
+	long *keys;
+	// Points to diff_context.weights: In xdl_recs_cmp, each of these
+	// chars is set to a "weight" -- usually 0 or 1 depending on whether
+	// that particular record has changed. So it's basically a bit vector
+	// made of chars
+	char *weights;
+};
+typedef struct diffdata diffdata;
+
 /*
  * Represents the inner variables required to run Myers O(ND)
  * diffing algorithm. This includes guiding information like
