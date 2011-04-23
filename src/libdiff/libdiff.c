599df831040af006f04f4c714a2c00ed2bd25e3a
Alex Clemmer <clemmer.alexander@gmail.com>
2011-04-23 15:13:24 -0600

    Add method that emits the hunk header

--- src/libdiff/libdiff.c
+++ src/libdiff/libdiff.c
@@ -114,6 +114,56 @@ int xdl_num_out(char *out, long val) {
 }
 
 
+int xdl_emit_hunk_hdr(long s1, long c1, long s2, long c2,
+		const char *func, long funclen, git_diff_callback *ecb) {
+	int nb = 0;
+	diff_mem_data mb;
+	char buf[128];
+
+	memcpy(buf, "@@ -", 4);
+	nb += 4;
+
+	nb += xdl_num_out(buf + nb, c1 ? s1: s1 - 1);
+
+	if (c1 != 1) {
+		memcpy(buf + nb, ",", 1);
+		nb += 1;
+
+		nb += xdl_num_out(buf + nb, c1);
+	}
+
+	memcpy(buf + nb, " +", 2);
+	nb += 2;
+
+	nb += xdl_num_out(buf + nb, c2 ? s2: s2 - 1);
+
+	if (c2 != 1) {
+		memcpy(buf + nb, ",", 1);
+		nb += 1;
+
+		nb += xdl_num_out(buf + nb, c2);
+	}
+
+	memcpy(buf + nb, " @@", 3);
+	nb += 3;
+	if (func && funclen) {
+		buf[nb++] = ' ';
+		if (funclen > sizeof(buf) - nb - 1)
+			funclen = sizeof(buf) - nb - 1;
+		memcpy(buf + nb, func, funclen);
+		nb += funclen;
+	}
+	buf[nb++] = '\n';
+
+	mb.data = buf;
+	mb.size = nb;
+	if (ecb->out_func(ecb->payload, &mb, 1) < 0)
+		return -1;
+
+	return 0;
+}
+
+
 // TODO: THIS IS A DIRECT PORT FROM xdiff/xprepare.c
 // PORT IT PROPERLY
 static void free_ctx(data_context *ctx) {
