03704d0621a68bc63dfa0bd0a63d19665643dc99
Alex Clemmer <clemmer.alexander@gmail.com>
2011-04-29 20:42:38 -0600

    Add include.h, used to include common structs and stuff

---
+++ src/libdiff/include.h
@@ -0,0 +1,17 @@
+/*
+ * include.h - Collection of miscellaneous types and things used
+ * internally in libdiff: common header files, structures, and
+ * so on.
+ */
+#ifndef INCLUDE_include_h__
+#define INCLUDE_include_h__
+
+#include "libdiff.h"
+
+
+
+struct diff_env {
+};
+
+
+#endif /* INCLUDE_include_h__ */
