d043a84642882ae5809be26a6550ce5050894041
Alex Clemmer <clemmer.alexander@gmail.com>
2011-04-29 21:12:45 -0600

    Add environment.h

---
+++ src/libdiff/environment.h
@@ -0,0 +1,16 @@
+/*
+ * environment.h - Holds miscellaneous builders, destructors, and
+ * and allocators that make life easier for users of libdiff.
+ */
+#ifndef INCLUDE_environment_h__
+#define INCLUDE_environment_h__
+
+#include "include.h"
+
+
+
+int init_diff_env(struct diff_env *env);
+
+
+
+#endif /* INCLUDE_utils_h__ */
