c9abbf37f15c5b6b4215e84fe9a7c3fe08e10daf
Alex Clemmer <clemmer.alexander@gmail.com>
2011-04-29 20:42:14 -0600

    Add file comment

--- src/libdiff/libdiff.h
+++ src/libdiff/libdiff.h
@@ -1,3 +1,9 @@
+/*
+ * libdif.h - Houses all functions that are intended to be directly
+ * accessible by all of libgit2. Includes all data structures needed
+ * to call and run diff. General rule: if it's not in here, it's not
+ * supposed to be exported (probably).
+ */
 #ifndef INCLUDE_libdiff_h__
 #define INCLUDE_libdiff_h__
 
