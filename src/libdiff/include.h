c2e6f39634f7bff8d143c07742f148bbb52fb30a
Alex Clemmer <clemmer.alexander@gmail.com>
2011-06-02 04:34:56 -0400

    Comments for internal libdiff struct

--- src/libdiff/include.h
+++ src/libdiff/include.h
@@ -22,12 +22,29 @@
 
 
 
+// Everything needed to perform diff
 struct diff_env {
+
+	// Content to diff; we will produce the minimum number of
+	// changes req'd to change diffme1 into diffme2
 	struct diff_mem *diffme1, *diffme2;
+
+	// Arrays of records; obtained by processing diffme1 and
+	// diffme2, respectively
 	struct record *rcrds1, *rcrds2;
+
+	// Number of records for diffme1 and diffme2, respectively
 	size_t num_rcrds1, num_rcrds2;
+
+	// Quickly-computed guess of number of records in diffme1 & 2
 	size_t rcrds_guess1, rcrds_guess2;
-	struct edit *ses_mem, *ses_head, *ses_tail;
+
+	// SES = Shortest Edit Script; array of changes req'd to
+	// change diffme1 into diffme2.
+	struct edit *ses_mem;
+
+	// Head and tail of SES
+	struct edit *ses_head, *ses_tail;
 };
 
 
