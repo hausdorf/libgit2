40a7d8f5ae1f14b49dfd94feac6ca9cfc6f041fb
Alex Clemmer <clemmer.alexander@gmail.com>
2011-06-02 04:30:02 -0400

    Add comments to public-facing structs

--- src/libdiff/libdiff.h
+++ src/libdiff/libdiff.h
@@ -13,25 +13,45 @@
 
 
 
+// Holds data we're diffing
 struct diff_mem {
 	char *data;
 	size_t size;
 };
 
 
+// An array of these makes up an edit script; basically they tell
+// you the steps required to transform one set of content into
+// another, e.g., ab -> b via DELETION of the first 'a'.
 struct edit {
+
+	// OPTIONAL: The record to insert is included if INSERTION
 	struct record *rcrd;
+
+	// OPTIONAL: The next edit in the script; NULL if last edit
 	struct edit *next;
+
+	// Type of edit -- INSERTION, DELETION, or END_OF_SCRIPT
 	unsigned char edit;
+
+	// Var names _directly_ from the Myers paper
 	size_t x;
 	size_t y;
 	size_t k;
 };
 
 
+
+// Corresponds to lines in human-readable content
 struct record {
+
+	// Where in a diff_mem instance the record exists; in
+	// human-readable content, this usually is a span of chars,
+	// e.g., from diff_mem->data[12] to diff_mem->data[27].
 	unsigned long start;
 	unsigned long end;
+
+	// The hash of the record; used for quick comparisons
 	unsigned long hash;
 };
 
