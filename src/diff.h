008cec94efc1992eb4ba84fd951aed9c7617f0ab
Alex Clemmer <clemmer.alexander@gmail.com>
2011-06-02 05:56:05 -0400

    Add flag to designate diff type in the diff_conf

--- src/diff.h
+++ src/diff.h
@@ -3,10 +3,35 @@
 
 #include "git2/diff.h"
 
+// "STANDARD" diff command
+// diffs working directory against index
+#define DIFF_IDX_WD 0
+
+// git diff <commit>
+// diffs working directory against <commit>
+#define DIFF_WD_CMT 1
+
+// git diff --cached
+// diffs index against the HEAD commit
+#define DIFF_IDX_HEAD 2
+
+// git diff --cached <commit>
+// diffs index against <commit>
+#define DIFF_IDX_CMT 3
+
+// git diff <commit> <commit>
+// diffs second commit against first commit
+#define DIFF_CMT_CMT 4
+
+// git diff <commit>...<commit>
+// tricky: diffs between the commit that both commits share as
+// an ancestor to the second commit
+#define DIFF_CMT_TO_CMT 5
+
 
 
 struct git_diff_conf {
-	unsigned long flags;
+	unsigned char diff_type;
 };
 
 
